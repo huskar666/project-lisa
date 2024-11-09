6426c26bb6bdf5861060ee65c3d689d4e41ae460
huskar666 <148646923+huskar666@users.noreply.github.com>
2024-11-09 20:35:39 +0200

    js delete

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const burgerMenu = document.querySelector(".burger-menu");
-    const mobileNav = document.querySelector(".mobile-nav");
-
-    burgerMenu.addEventListener("click", function() {
-        mobileNav.classList.toggle("open");
-    });
-});
